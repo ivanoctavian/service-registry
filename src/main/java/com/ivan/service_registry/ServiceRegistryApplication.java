bf5d87d405bf50668cfca4ffbbd566663743db56
octavianivan <i.octavian900@gmail.com>
2025-04-04 00:13:57 +0300

    Added eureka config. Refactored from properties to yml.

--- src/main/java/com/ivan/service_registry/ServiceRegistryApplication.java
+++ src/main/java/com/ivan/service_registry/ServiceRegistryApplication.java
@@ -2,11 +2,14 @@ package com.ivan.service_registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ServiceRegistryApplication {
 
 	public static void main(String[] args) {
+		System.out.println("HELLOW WORLD");
 		SpringApplication.run(ServiceRegistryApplication.class, args);
 	}
 
